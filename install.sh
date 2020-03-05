1d695b4ecc8bc6d78ce33f65dc79620f5ef03309
gx1 <g.per45@gmail.com>
2020-03-05 19:08:25 +0100

    Added 256 colors for tmux, added backup of bashrc

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@ sudo apt-get install -y vim git wget tmux
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 source ~/.bashrc
+cp ~/.bashrc ~/.bashrc.bak
+echo "Bashrc backed up"
 cp ./gx1_bashrc ~/.bashrc
 cp ./gx1_vimrc  ~/.vim_runtime/my_configs.vim
 cp gx1_tmux ~/.tmux.conf
